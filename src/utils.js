45c50ab5f778752841ee9d9ee0f87b62da861b1b
Eduard-Kirilov <kev987.free@ya.ru>
2020-05-05 20:46:18 +0500

    path fix

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,7 @@ exports.getImages = async (req, res) => {
   try {
     if (req.params.id && req.query.w && req.query.h) {
       const dir = path.join(__dirname, 'images/media')
-      const image = `${dir}\\${req.params.id}`;
+      const image = path.join(dir, req.params.id)
       const resizeImage = await sharp(image)
         .resize(Number(req.query.w), Number(req.query.h))
         .png()
@@ -15,7 +15,6 @@ exports.getImages = async (req, res) => {
     return res.status(401).send('Bad request!');
   }
   catch (err) {
-    console.log(err)
-    return res.status(404).send(`No such file or directory`);
+    throw err;
   }
 }
